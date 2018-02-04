7e756afd40da67bd8245163e814707ed1cd10006
oslboreal <oslboreal@gmail.com>
2018-02-04 13:32:54 -0300

    Comentarios del proyecto.

--- ws/index.php
+++ ws/index.php
@@ -1,4 +1,15 @@
 <?php
-echo 'Web site under construction.<br>Thanks you for understand.';
+
+/*
+
+     Web services - En este fichero podrán ser configurado los servicios principales de nuestra aplicación. 
+     - https://www.github.com/oslboreal/sistemaEstacionamiento 
+     - Vallejo Juan Marcos - 2018 - This code is open to te world, you can use, study, modify and redistribute it. 
+
+*/
+
+// Llamada a las bibliotecas necesarias. 
+
+// Servicios. 
 
 ?>
